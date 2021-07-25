d0a392cc56b6b2576151de6533bd43ead0ff6413
justinmartinfill <justinmartinfill@gmail.com>
2021-07-25 16:59:26 -0400

    first solo, test on lines 22-25 of objects.specs.ts

--- tests/unit/01.2_transverse_object/objects.spec.ts
+++ tests/unit/01.2_transverse_object/objects.spec.ts
@@ -2,25 +2,26 @@
 // https://www.w3schools.com/js/js_objects.asp  // Simple explanation
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects // in depth really good read! 🤓
 describe('transverse objects and object reference', () => {
-  const pizza = null
+  const pizza = {size:'large',crust:'thin', topping:'pepperoni'}
+  
 
-  test.skip('there is a variable "pizza" will an empty object', () => {
+  test('there is a variable "pizza" that will be an empty object', () => {
     expect(pizza).not.toBeNull()
   })
 
-  test.skip('pizza object has property size and size is set to a string "large"', () => {
-    // expect(pizza.size).not.toBeNull()
-    // expect(pizza.size).toBe('large')
+  test('pizza object has property size and size is set to a string "large"', () => {
+     expect(pizza.size).not.toBeNull()
+     expect(pizza.size).toBe('large')
   })
 
-  test.skip('pizza object has property crust and crust is set to a string "thin"', () => {
-    // expect(pizza.crust).not.toBeNull()
-    // expect(pizza.crust).toBe('thin')
+  test('pizza object has property crust and crust is set to a string "thin"', () => {
+     expect(pizza.crust).not.toBeNull()
+     expect(pizza.crust).toBe('thin')
   })
 
-  test.skip('pizza object has property topping and topping is set to a string "pepperoni"', () => {
-    // expect(pizza.topping).not.toBeNull()
-    // expect(pizza.topping).toBe('pepperoni')
+  test('pizza object has property topping and topping is set to a string "pepperoni"', () => {
+     expect(pizza.topping).not.toBeNull()
+     expect(pizza.topping).toBe('pepperoni')
   })
 
   test.skip('pizza object has a method cook', () => {
