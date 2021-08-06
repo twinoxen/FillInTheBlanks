e1327629fcc2161215540d8e4bf4b3a555228483
twinoxen <1854037+twinoxen@users.noreply.github.com>
2021-08-06 08:29:17 -0400

    clean boolean test

--- tests/unit/06_boolean/boolean.spec.ts
+++ tests/unit/06_boolean/boolean.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-self-compare */
 /* eslint-disable no-constant-condition */
 
 // A JavaScript Boolean represents one of two values: true or false.
@@ -24,13 +25,13 @@ describe('boolean', () => {
     // expect(myVar).toBe() // what value will myVar have? Put the correct answer in the toBe() method
   })
 
-  test('boolean can be derived by expressions', () => {
+  test.skip('boolean can be derived by expressions', () => {
     // An expression is a sentence with a minimum of two numbers and at least one math operation
     // fill in the toBe() methods to make the test pass
     
-    expect(540 > 49.04040496988).toBe()
-    expect(200 < 1000).toBe()
-    expect(93 <= 93).toBe()
-    expect(1 >= 1.0304394).toBe()
+    // expect(540 > 49.04040496988).toBe()
+    // expect(200 < 1000).toBe()
+    // expect(93 <= 93).toBe()
+    // expect(1 >= 1.0304394).toBe()
   })
 })
