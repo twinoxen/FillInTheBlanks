31749802a9c4dff679394a603a741b0c0a5f1b7f
twinoxen <1854037+twinoxen@users.noreply.github.com>
2021-08-06 08:28:38 -0400

    add tests for boolean

---
+++ tests/unit/06_boolean/boolean.spec.ts
@@ -0,0 +1,36 @@
+/* eslint-disable no-constant-condition */
+
+// A JavaScript Boolean represents one of two values: true or false.
+// https://www.w3schools.com/js/js_booleans.asp
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean
+describe('boolean', () => {
+  it.skip('is true', () => {
+    let myVar
+
+    if(true) {
+      myVar = true
+    }
+
+    // expect(myVar).toBe() // what value will myVar have? Put the correct answer in the toBe() method
+  })
+
+  it.skip('is false', () => {
+    let myVar
+
+    if(true) {
+      myVar = false
+    }
+
+    // expect(myVar).toBe() // what value will myVar have? Put the correct answer in the toBe() method
+  })
+
+  test('boolean can be derived by expressions', () => {
+    // An expression is a sentence with a minimum of two numbers and at least one math operation
+    // fill in the toBe() methods to make the test pass
+    
+    expect(540 > 49.04040496988).toBe()
+    expect(200 < 1000).toBe()
+    expect(93 <= 93).toBe()
+    expect(1 >= 1.0304394).toBe()
+  })
+})
